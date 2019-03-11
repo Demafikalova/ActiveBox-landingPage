ec88d544c421314f70604e21506fd27c32d9b8a9
Demafikalova <krzysior00@gmail.com>
2019-03-11 11:19:56 +0100

    Added scrolling features and changed some code

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,16 +1,31 @@
-$(document).ready(function () {
-    $(window).on('scroll', function() {
-        var scroll = $(window).scrollTop();
-        console.log(scroll);
-        if (scroll >= 50) {
-            $('.navbar').addClass('navbar-fixed');
-        } else {
-            $('.navbar').removeClass('navbar-fixed');
+$(document).ready(function() {
+  $(window).on("scroll", function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 50) {
+      $(".navbar").addClass("navbar-fixed");
+    } else {
+      $(".navbar").removeClass("navbar-fixed");
+    }
+  });
+   // Smooth Scrolling
+    $("#main-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            const hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                window.location.hash = hash;
+            });
         }
     });
 
-    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
-        event.preventDefault();
-        $(this).ekkoLightbox();
-    });
+
+  $(document).on("click", '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+  });
 });
